9d2a2c35c54ecb2c8744947e87bc2e9a97701689
DanielSebasCM <danielsebascm@hotmail.com>
2022-05-23 17:25:30 -0500

    Switch cases for file input

--- main.cpp
+++ main.cpp
@@ -20,6 +20,96 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-  return 0;
+int main(int argc, char* argv []) {
+
+    ifstream inputFile;
+    ofstream outputFile;
+
+    if (argc != 3)
+    {
+        cout << "Uso: " << argv[0] << " Input.txt Output.txt";
+        return -1;
+    }
+
+    inputFile.open(argv[1]);
+    if (inputFile.fail())
+    {
+        cout << "No se pudo abrir el archivo de entrada.";
+        return -1;
+    }
+
+    outputFile.open(argv[2]);
+    if (outputFile.fail())
+    {
+        cout << "No se pudo abrir el archivo de salida.";
+        return -1;
+    }
+
+
+    int C, O, N, inst, age, id_op, discount_rate, id_c1, id_c2, minutes, quantity;
+    double limit, talking_charge, message_cost, network_charge, amount;
+    string name;
+    int type;
+
+    inputFile >> C >> O >> N;
+
+    Customer customers[C];
+    Operator operators[O];
+
+    int op_count = 0;
+    int customer_count = 0;
+
+    for (int i = 0; i < N; i++) {
+        inputFile >> inst;
+
+        switch (inst) {
+            case 1: 
+                inputFile >> name >> age >> id_op >> limit;
+                customers[customer_count] = Customer(customer_count, name, age, &operators[id_op], limit);
+                customer_count ++;
+                break;
+            case 2: 
+                inputFile >> type >> talking_charge >> message_cost >> network_charge >> discount_rate;
+                if (type == 1) {
+                  operators[op_count] = VoxOperator(op_count, talking_charge, message_cost, network_charge, discount_rate, VOX);
+                  op_count ++;
+                }
+                else if (type == 2) {
+                  operators[op_count] = InternetOperator(op_count, talking_charge, message_cost, network_charge, discount_rate, INTERNET);
+                  op_count ++;
+                }
+                break;
+            case 3: 
+                inputFile >> id_c1 >> id_c2 >> minutes;
+                customers[id_c1].talk(minutes, customers[id_c2]);
+                break;
+            case 4: 
+                inputFile >> id_c1 >> id_c2 >> quantity;
+                customers[id_c1].message(quantity, customers[id_c2]);
+                break;
+            case 5: 
+                inputFile >> id_c1 >> amount;
+                customers[id_c1].connection(amount);
+                break;
+            case 6: 
+                inputFile >> id_c1 >> amount;
+                customers[id_c1].getBill()->pay(amount);
+                break;
+            case 7:
+                inputFile >> id_c1 >> id_op;
+                customers[id_c1].setOperator(&operators[id_op]);
+                break;
+            case 8:
+                inputFile >> id_c1 >> limit;
+                customers[id_c1].getBill()->changeTheLimit(limit);
+            default:
+                break;
+        }
+    }
+
+    inputFile.close();
+    outputFile.close();
+
+
+    return 0;
 }
