d87685e1d1a860d42725cd8b0bc6ed9a2c7463c4
DanielSebasCM <danielsebascm@hotmail.com>
2022-05-23 11:46:18 -0500

    Interner first implementation

--- internet.h
+++ internet.h
@@ -23,4 +23,43 @@ public:
   double calculateNetworkCost(double);
 };
 
+InternetOperator::InternetOperator(int i, double talking, double message, double network, int discount, OperatorType typ)
+            :Operator(i, talking, message, network, discount, typ){}
+
+InternetOperator::InternetOperator(const InternetOperator &other)
+            :Operator(other){}
+
+double InternetOperator::calculateTalkingCost(int minute, int age) {
+  double cost = 0;
+  if (minute > 0 && age > 0) {
+    cost = minute * talkingCharge;
+    if (minute < 2) {
+      cost -= cost  *  discountRate/100;
+    }
+  }
+
+  return cost;
+}
+
+double InternetOperator::calculateMessageCost(int quantity, int thisOpId, int otherOpId) {
+  double cost = 0;
+  if (quantity > 0) {
+    cost = quantity * messageCost;
+    if (quantity < 3) {
+      cost -= cost  *  discountRate/100;
+    }
+  }
+
+  return cost;
+}
+
+double InternetOperator::calculateNetworkCost(double ammount) {      
+  double cost = 0;
+  if (ammount + totalInternetUsage > LIMITGB) {
+    cost = ammount * networkCharge;
+  }
+
+  return cost;
+}    
+
 #endif
